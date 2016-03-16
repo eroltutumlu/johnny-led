e2dad07cbf8df609e071bf21735647bbb04d1f91
ErolTUTUMLU <eroltutumlu@gmail.com>
2016-03-16 18:08:03 +0200

    I changes the structure of codes

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
-var johnnyFive = require('johnny-five');
-var Firebase = require('Firebase');
-
-var board = new johnnyFive.Board(),
-    button, led;
-
-var firebaseRef = new Firebase("https://johnny-five-led.firebaseio.com/eroltutumlu");
-
-board.on("ready", function(){
-
-    button = johnnyFive.Button(2);
-    led = new johnnyFive.Led(7);
+(function(){
     
-    button.on('up', function(){
+    var johnnyFives = {
         
-        led.on();
-        firebaseRef.set("up");
+        setModules: function(){
+            this.johnnyFive = require('johnny-five');
+            this.firebase = require('Firebase');
+            this.firebaseRef = new this.firebase("https://johnny-five-led.firebaseio.com/eroltutumlu");
+        },
         
-    });
-    
-    
-    button.on('down', function(){
-        led.off();
-        firebaseRef.set("down");
+        init: function(){
+            this.setModules();
+        },
         
-    });
+        boardReady: function(){
+            this.board = new this.johnnyFive.Board();
+            
+            var self = this;
+            
+            this.board.on("ready", function(){
+                this.button = self.johnnyFive.Button(2);
+                this.led = new self.johnnyFive.Led(7);
+                
+                var board = this;
+                
+                this.button.on('up', function(){
+                    board.led.on();
+                    self.firebaseRef.set("up");
+
+                });
+                this.button.on('down', function(){
+                    board.led.off();
+                    self.firebaseRef.set("down");
+
+                });
+                
+                
+                self.firebaseRef.on("value", function(snapshot){
+
+                    var buttonState = snapshot.val();
+                    
+                    if(buttonState == "up"){
+                        board.led.on();
+                    }else{
+                        board.led.off();
+                    }
+                });
+                
+            });
+                
+        }  
     
-    
-    firebaseRef.on("value", function(snapshot){
+    }
+
+    johnnyFives.init();
+    johnnyFives.boardReady();
         
-        var buttonState = snapshot.val();
-        if(buttonState == "up"){
-            led.on();
-        }else{
-            led.off();
-        }
-    })
     
-});
+}());
+
--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 (function(){
     var firebaseRef = new Firebase("https://johnny-five-led.firebaseio.com/eroltutumlu");
     
+    document.getElementById('btnLedOpen').addEventListener("click", upButton);
+    document.getElementById('btnLedClose').addEventListener("click", downButton);
     
-    document.getElementById('btnLedOpen').addEventListener("click", myFunction);
-    document.getElementById('btnLedClose').addEventListener("click", myFunction2);
-    
-    function myFunction() {
+    function upButton() {
         firebaseRef.set("up");
+        
+        var ledStatusDiv = document.querySelector('#ledStatus');
+        ledStatusDiv.className = "bg-danger";
+        ledStatusDiv.innerHTML = "<h4>Open</h4>";
+        
+        
     }
     
-    function myFunction2() {
+    function downButton() {
         firebaseRef.set("down");
+        
+        var ledStatusDiv = document.querySelector('#ledStatus');
+        ledStatusDiv.className = "bg-success";
+        ledStatusDiv.innerHTML = "<h4>Close</h4>";
+        
+        
     }
+    
+    
     
 
-}())
+}());
